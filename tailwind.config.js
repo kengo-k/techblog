93524eda406324df30e51909d90bdcc0c8e0a6de
kengo-k <31839234+kengo-k@users.noreply.github.com>
2025-07-13 23:42:28 +0900

    add local tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./themes/JustPlain/layouts/**/*.html",
+    "./content/**/*.md",
+    "./static/js/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
